2bc0df18038fba5a273a49b4bbe88e676d7581f0
PCejrowski <pcejrowski@gmail.com>
2015-10-04 18:41:15 +0200

    CLI się wykonuje i tworzy raport, który jest pusty

--- src/main/scala/protobufui/cli/CliMain.scala
+++ src/main/scala/protobufui/cli/CliMain.scala
@@ -6,7 +6,7 @@ import javax.xml.bind.{Marshaller, JAXBContext}
 
 import akka.actor.Props
 import ipetoolkit.workspace.WorkspaceManagement.LoadWorkspace
-import ipetoolkit.workspace.WorkspacePersistence.Loaded
+import ipetoolkit.workspace.WorkspacePersistence.{Load, Loaded}
 import ipetoolkit.workspace.{WorkspacePersistence, WorkspaceManager}
 import protobufui.gui.workspace.base.RootEntry
 import protobufui.service.source.ClassesLoader.Put
@@ -23,11 +23,12 @@ import scala.util.{Failure, Success}
 
 object CliMain {
   def start(config:Config)={
+    Globals.setProperty(Globals.Keys.workspaceRoot,config.workspace.getParentFile.getAbsolutePath)
     val workspacePersistence = Main.actorSystem.actorOf(Props(classOf[WorkspacePersistence]))
 
     implicit val timeout = akka.util.Timeout(30 second)
 
-    val loadedMsg = Await.result((workspacePersistence ? LoadWorkspace(config.workspace.getAbsolutePath)).mapTo[Loaded], 30 seconds)
+    val loadedMsg = Await.result((workspacePersistence ? Load(config.workspace)).mapTo[Loaded], 30 seconds)
     loadedMsg match {
       case Loaded(Success(rootEntry:RootEntry)) =>
         runTests(config, rootEntry)
@@ -43,27 +44,27 @@ object CliMain {
 
 
     implicit val timeout = akka.util.Timeout(30 second)
-
-    Globals.setProperty(Globals.Keys.workspaceRoot,config.workspace.getAbsolutePath)
-    val classesLoader = Main.actorSystem.actorOf(Props(new ClassesLoader(config.workspace)))
+    val classesLoader = Main.actorSystem.actorOf(Props(new ClassesLoader(config.workspace.getParentFile)))
 
     classesLoader ! Put(config.load)
 
-    Thread.sleep(20000)
+    Thread.sleep(10000)
     val suitesRunner = Main.actorSystem.actorOf(Props(new SuitesRunner(config.suites, rootEntry)))
     val casesRunner = Main.actorSystem.actorOf(Props(new CasesRunner(config.cases, rootEntry)))
     val suitesResultsFuture = (suitesRunner ? RunSuites).mapTo[List[TestSuiteResult]]
     val casesResultsFuture = (casesRunner ? RunCases).mapTo[List[TestCaseResult]]
     val suitesResults = Await.result(suitesResultsFuture,30 seconds)
-    val casesResults = Await.result(casesResultsFuture,30 seconds)
+    //val casesResults = Await.result(casesResultsFuture,30 seconds)
     val jaxbResult = TestReportCreator.createReport(suitesResults) // todo: dla cas�w
 
 
-    val reportsRoot = Globals.getProperty(Globals.Keys.workspaceRoot) + File.separator + "reports"
+    val reportsRoot = Globals.getProperty(Globals.Keys.workspaceRoot).get + File.separator + "reports"
 
     val jaxbContext = JAXBContext.newInstance(classOf[Testsuites])
     val jaxbMarshaller = jaxbContext.createMarshaller()
     jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true)
-    jaxbMarshaller.marshal(jaxbResult, new File(reportsRoot,LocalDateTime.now.toString))
+    val reportFile: File = new File(reportsRoot, LocalDateTime.now.toString.replace(":","_").replace(".","_")+".xml")
+    reportFile.getParentFile.mkdirs()
+    jaxbMarshaller.marshal(jaxbResult, reportFile)
   }
 }
